e6a55656c6b17280c8a67d8200b168ff4c8ce549
Kirill Novik <thenavik1337@gmail.com>
2021-07-02 12:52:46 +0300

    Add comments for method

--- DevelopmentAndBuildTools/DevelopmentAndBuildTools/EntryPoint.cs
+++ DevelopmentAndBuildTools/DevelopmentAndBuildTools/EntryPoint.cs
@@ -2,11 +2,58 @@
 
 namespace DevelopmentAndBuildTools
 {
-    class Program
+    class EntryPoint
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                Console.WriteLine(GetMaxCountOfUniqueSequenceOfLetters(args[0]);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+
+        /// <summary>
+        /// Method which calculating max amount of unique symbols in string
+        /// </summary>
+        /// <param name="str">Calculated string</param>
+        /// <returns>Max count of unique symbols</returns>
+        public static int GetMaxCountOfUniqueSequenceOfLetters(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                return 0;
+            }
+
+            int count = 1;
+            int maxCount = 1;
+
+            for(int i = 0; i < str.Length-1; i++)
+            {
+                if (str[i] != str[i + 1])
+                {
+                    count++;
+                }
+
+                if (str[i] == str[i + 1])
+                {
+                    if (count > maxCount)
+                    {
+                        maxCount = count;
+                    }
+                    count = 1;
+                }
+            }
+
+            if (count > maxCount)
+            {
+                maxCount = count;
+            }
+
+            return maxCount;
         }
     }
 }
