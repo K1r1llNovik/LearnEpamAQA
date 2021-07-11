03949acfe1a13e9987548fcdbabe2933e227bac8
Kirill Novik <thenavik1337@gmail.com>
2021-07-11 17:32:58 +0300

    Add class bus

---
+++ OOP/OOP/Vehicles/Bus.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP
+{
+    public class Bus : Vehicle
+    {
+        private byte _maximumNumberOfPassengers;
+
+        /// <summary>
+        /// Get and set maximum number of passengers
+        /// </summary>
+        public byte MaximumNumberOfPassengers
+        {
+            get
+            {
+                return _maximumNumberOfPassengers;
+            }
+
+            set
+            {
+                if(value < 0)
+                {
+                    throw new ArgumentException(nameof(_maximumNumberOfPassengers));
+                }
+                _maximumNumberOfPassengers = value;
+            }
+        }
+
+        /// <summary>
+        /// Constructor initializes class fields
+        /// </summary>
+        /// <param name="maximumNumberOfPassengers"></param>
+        /// <param name="chassis"></param>
+        /// <param name="engine"></param>
+        /// <param name="transmission"></param>
+        public Bus(byte maximumNumberOfPassengers, Chassis chassis, Engine engine, Transmission transmission) : base(chassis, engine, transmission)
+        {
+            MaximumNumberOfPassengers = maximumNumberOfPassengers;
+        }
+
+        public override string GetInfo()
+        {
+            return base.GetInfo() + $"Maximum number of passengers: {MaximumNumberOfPassengers}";
+        }
+    }
+}
--- OOP/OOP/Vehicles/Truck.cs
+++ OOP/OOP/Vehicles/Truck.cs
@@ -2,9 +2,51 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace OOP.Vehicles
+namespace OOP
 {
-    class Truck
+    public class Truck : Vehicle
     {
+        private double _maximumLiftingCapacity;
+
+        /// <summary>
+        /// Get and set maximum lifting capacity
+        /// </summary>
+        public double MaximumLiftingCapacity
+        {
+            get
+            {
+                return _maximumLiftingCapacity;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException(nameof(_maximumLiftingCapacity));
+                }
+                _maximumLiftingCapacity = value;
+            }
+        }
+
+        /// <summary>
+        /// Constructor initializes class fields
+        /// </summary>
+        /// <param name="maximumLiftingCapacity"></param>
+        /// <param name="chassis"></param>
+        /// <param name="engine"></param>
+        /// <param name="transmission"></param>
+        public Truck(double maximumLiftingCapacity, Chassis chassis, Engine engine, Transmission transmission) : base(chassis, engine, transmission)
+        {
+            MaximumLiftingCapacity = _maximumLiftingCapacity;
+        }
+
+        /// <summary>
+        /// Method that returns all information about Truck
+        /// </summary>
+        /// <returns></returns>
+        public override string GetInfo()
+        {
+            return base.GetInfo() + $"Maximum lifting capacity: {MaximumLiftingCapacity}";
+        }
     }
 }
