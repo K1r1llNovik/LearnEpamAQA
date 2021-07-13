0bc3f15bd31ba1f2929e18124f7b702ce6534dd3
Kirill Novik <thenavik1337@gmail.com>
2021-07-13 14:55:49 +0300

    Add abstract class flying object

---
+++ InterfacesAndAbstractClasses/InterfacesAndAbstractClasses/Flying objects/FlyingObject.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfacesAndAbstractClasses
+{
+    public abstract class FlyingObject
+    {
+        /// <summary>
+        /// Method that get and set current postion of flying object
+        /// </summary>
+        public Coordinate Postion { get; set; }
+
+        /// <summary>
+        /// Constructor for flying object
+        /// </summary>
+        /// <param name="position"></param>
+        public FlyingObject(Coordinate position)
+        {
+            Postion = position;
+        }
+
+
+    }
+}
