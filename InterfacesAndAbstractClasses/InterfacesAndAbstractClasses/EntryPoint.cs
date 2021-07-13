0c8ae243f111048fb0798fff3a862fc083d654b6
Kirill Novik <thenavik1337@gmail.com>
2021-07-13 18:00:31 +0300

    Add some examples in entry point

--- InterfacesAndAbstractClasses/InterfacesAndAbstractClasses/EntryPoint.cs
+++ InterfacesAndAbstractClasses/InterfacesAndAbstractClasses/EntryPoint.cs
@@ -6,7 +6,17 @@ namespace InterfacesAndAbstractClasses
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Bird bird = new Bird(new Coordinate(10, 5, 3));
+            Plane plane = new Plane(1300, new Coordinate(10, 3, 6));
+            Drone drone = new Drone(new Coordinate(115, 44, 3));
+            Coordinate coordinate = new Coordinate(115, 44, 3);
+            Console.WriteLine(coordinate.GetDistance(new Coordinate(550, 320, 150)));
+
+            Console.WriteLine(plane.GetFlyTime(new Coordinate(1150, 780, 980)));
+
+            Console.WriteLine(drone.GetFlyTime(new Coordinate(550, 320, 150)));
+
+            Console.WriteLine(bird.GetFlyTime(new Coordinate(20, 13, 50)));
         }
     }
 }
