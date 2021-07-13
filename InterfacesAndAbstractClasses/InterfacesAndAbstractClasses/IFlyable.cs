c4ba41516ecf88c88dc9e07736b9afe4510ed74c
Kirill Novik <thenavik1337@gmail.com>
2021-07-13 14:50:07 +0300

    Add interface IFlyable

---
+++ InterfacesAndAbstractClasses/InterfacesAndAbstractClasses/IFlyable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfacesAndAbstractClasses
+{
+    interface IFlyable
+    {
+        void FlyTo(Coordinate newCoordinate);
+
+        DateTime GetFlyTime(Coordinate newCoordinate);
+    }
+}
