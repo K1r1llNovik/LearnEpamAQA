405d539764ae54542b16d8e2028dd302ec237b95
Kirill Novik <thenavik1337@gmail.com>
2021-08-17 13:50:10 +0300

    add AddException in CarPark

--- OOP/OOP/CarPark.cs
+++ OOP/OOP/CarPark.cs
@@ -8,9 +8,37 @@ namespace OOP
     {
         public List<Vehicle> Vehicles { get; set; }
 
+        /// <summary>
+        /// Constructor initilize class fields
+        /// </summary>
+        /// <param name="vehicles"></param>
         public CarPark(List<Vehicle> vehicles)
         {
             Vehicles = vehicles;
+        }
+
+        /// <summary>
+        /// Add vehicle to the collection
+        /// </summary>
+        /// <param name="vehicle"></param>
+        public void Add(Vehicle vehicle)
+        {
+            if (IsContains(vehicle))
+            {
+                throw new AddException("Unable to add car model");
+            }
+
+            Vehicles.Add(vehicle);
+        }
+
+        /// <summary>
+        /// Determines if the collection contains an object
+        /// </summary>
+        /// <param name="vehicle"></param>
+        /// <returns></returns>
+        private bool IsContains(Vehicle vehicle)
+        {
+            return Vehicles.Select(v => v.Equals(vehicle)).Contains(true);
         }
     }
 }
