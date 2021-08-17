9d71c6118366284bf0c96943572b9c1b7d23b7ac
Kirill Novik <thenavik1337@gmail.com>
2021-08-17 15:27:25 +0300

    Add GetAutoByParameter method and exception

--- OOP/OOP/CarPark.cs
+++ OOP/OOP/CarPark.cs
@@ -40,5 +40,77 @@ namespace OOP
         {
             return Vehicles.Select(v => v.Equals(vehicle)).Contains(true);
         }
+
+        /// <summary>
+        /// Method which return list of vehicles selected by parameter
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <param name="value"></param>
+        /// <returns>List of selected vehicles</returns>
+        public List<Vehicle> GetAutoByParameter(string parameter, string value)
+        {
+            List<Vehicle> selectedVehicles = new List<Vehicle>();
+            switch (parameter.ToLower())
+            {
+                case "enginetype":
+                    selectedVehicles = (from v in Vehicles
+                                        where v.VehicleEngine.Type.ToString().Equals(value)
+                                        select v).ToList();
+                    break;
+
+                case "transmissiontype":
+                    selectedVehicles = (from v in Vehicles
+                                        where v.VehicleTransmission.Type.ToString().Equals(value)
+                                        select v).ToList();
+                    break;
+
+                case "numberofgears":
+                    selectedVehicles = (from v in Vehicles
+                                        where v.VehicleTransmission.NumberOfGears.ToString().Equals(value)
+                                        select v).ToList();
+                    break;
+
+                case "manufacter":
+                    selectedVehicles = (from v in Vehicles
+                                        where v.VehicleTransmission.Manufacturer.ToString().Equals(value)
+                                        select v).ToList();
+                    break;
+
+                case "serialnumber":
+                    selectedVehicles = (from v in Vehicles
+                                        where v.VehicleEngine.SerialNumber.ToString().Equals(value)
+                                        select v).ToList();
+                    break;
+
+                case "volume":
+                    selectedVehicles = (from v in Vehicles
+                                        where v.VehicleEngine.Volume.ToString().Equals(value)
+                                        select v).ToList();
+                    break;
+
+                case "capacity":
+                    selectedVehicles = (from v in Vehicles
+                                        where v.VehicleEngine.Capacity.ToString().Equals(value)
+                                        select v).ToList();
+                    break;
+
+                case "numberofwheels":
+                    selectedVehicles = (from v in Vehicles
+                                        where v.VehicleChassis.NumberOfWheels.ToString().Equals(value)
+                                        select v).ToList();
+                    break;
+
+                case "PermissibleLoad":
+                    selectedVehicles = (from v in Vehicles
+                                        where v.VehicleChassis.PermissibleLoad.ToString().Equals(value)
+                                        select v).ToList();
+                    break;
+                default:
+                    throw new GetAutoByParameterException("This parameter does not exist");
+            }
+            return selectedVehicles;
+        }
+
+
     }
 }
