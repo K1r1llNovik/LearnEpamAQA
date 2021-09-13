32f226a69ee57c9e23ff37624b409bb4bb5f299c
Kirill Novik <thenavik1337@gmail.com>
2021-09-13 19:49:44 +0300

    Refactoring

--- DesignPatterns/DesignPatterns/Program.cs
+++ DesignPatterns/DesignPatterns/Program.cs
@@ -10,7 +10,6 @@ namespace DesignPatterns
         {
             Invoker invoker = new Invoker();
             CommandReader commandReader = new CommandReader();
-            Car car1 = new Car("bmw", "x6", 3, 3000);
             Console.WriteLine("print -commands- to see a list of commands");
 
             while (true)
