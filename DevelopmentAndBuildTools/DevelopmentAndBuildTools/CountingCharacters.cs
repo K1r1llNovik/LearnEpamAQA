5fb26de63e502a93cdc9b0a6a7425d0cc1423c7f
Kirill Novik <thenavik1337@gmail.com>
2021-09-13 21:26:50 +0300

    Add class countingcharacters and methods

---
+++ DevelopmentAndBuildTools/DevelopmentAndBuildTools/CountingCharacters.cs
@@ -0,0 +1,129 @@
+﻿using System;
+
+namespace DevelopmentAndBuildTools
+{
+    public class CountingCharacters
+    {
+        /// <summary>
+        /// Method which calculating max amount of unique symbols in string
+        /// </summary>
+        /// <param name="str">Calculated string</param>
+        /// <returns>Max count of unique symbols</returns>
+        public int GetMaxCountOfUniqueSequenceOfLetters(string sequenceOfCounting)
+        {
+            if (string.IsNullOrEmpty(sequenceOfCounting))
+            {
+                return 0;
+            }
+
+            int count = 1;
+            int maxCount = 1;
+
+            for (int i = 0; i < sequenceOfCounting.Length - 1; i++)
+            {
+                if (sequenceOfCounting[i] != sequenceOfCounting[i + 1])
+                {
+                    count++;
+                }
+
+                if (sequenceOfCounting[i] == sequenceOfCounting[i + 1])
+                {
+                    if (count > maxCount)
+                    {
+                        maxCount = count;
+                    }
+                    count = 1;
+                }
+            }
+
+            return count > maxCount ? count : maxCount;
+        }
+
+        /// <summary>
+        /// Method which calculating max amount of the same latin letters
+        /// </summary>
+        /// <param name="sequenceOfCounting"></param>
+        /// <returns>Max amount of the same latin letters in string</returns>
+        public int GetMaxOfTheSameLatinLetters(string sequenceOfCounting)
+        {
+            if (string.IsNullOrEmpty(sequenceOfCounting))
+            {
+                return 0;
+            }
+
+            int count = 0;
+            int maxCount = 0;
+
+            for (int i = 0; i < sequenceOfCounting.Length - 1; i++)
+            {
+                if (IsLatinSymbol(sequenceOfCounting[i]) && count == 0)
+                {
+                    count = 1;
+                }
+
+                if (IsLatinSymbol(sequenceOfCounting[i]) && sequenceOfCounting[i] == sequenceOfCounting[i + 1])
+                {
+                    count++;
+                }
+                else
+                {
+                    if (count > maxCount)
+                    {
+                        maxCount = count;
+                    }
+                    count = 0;
+                }
+            }
+
+            if (count > maxCount)
+            {
+                maxCount = count;
+            }
+            return maxCount;
+        }
+
+        /// <summary>
+        /// Method which calculating max amount of the same digits in string
+        /// </summary>
+        /// <param name="sequenceOfCounting"></param>
+        /// <returns>Max amount of the same digits in string</returns>
+        public int GetMaxOfTheSameDigit(string sequenceOfCounting)
+        {
+            if (string.IsNullOrEmpty(sequenceOfCounting))
+            {
+                return 0;
+            }
+
+            int count = 0;
+            int maxCount = 0;
+
+            for (int i = 0; i < sequenceOfCounting.Length - 1; i++)
+            {
+                if (Char.IsDigit(sequenceOfCounting[i]) && count == 0)
+                {
+                    count = 1;
+                }
+
+                if (Char.IsDigit(sequenceOfCounting[i]) && sequenceOfCounting[i] == sequenceOfCounting[i + 1])
+                {
+                    count++;
+                }
+                else
+                {
+                    if (count > maxCount)
+                    {
+                        maxCount = count;
+                    }
+                    count = 0;
+                }
+            }
+            return count > maxCount ? count : maxCount;
+        }
+
+        private bool IsLatinSymbol(char symbol)
+        {
+            return (symbol >= 'A' && symbol <= 'Z') ||
+                (symbol >= 'a' && symbol <= 'z');
+        }
+    }
+}
--- DevelopmentAndBuildTools/DevelopmentAndBuildTools/EntryPoint.cs
+++ DevelopmentAndBuildTools/DevelopmentAndBuildTools/EntryPoint.cs
@@ -2,58 +2,19 @@
 
 namespace DevelopmentAndBuildTools
 {
-    class EntryPoint
+    public class EntryPoint
     {
         static void Main(string[] args)
         {
             try
             {
-                Console.WriteLine(GetMaxCountOfUniqueSequenceOfLetters(args[0]));
+                CountingCharacters counter = new CountingCharacters();
+                Console.WriteLine(counter.GetMaxOfTheSameDigit(args[0]));
             }
-            catch (Exception exception)
+            catch(Exception ex)
             {
-                Console.WriteLine(exception.Message);
+                Console.WriteLine(ex.Message);
             }
-        }
-
-        /// <summary>
-        /// Method which calculating max amount of unique symbols in string
-        /// </summary>
-        /// <param name="str">Calculated string</param>
-        /// <returns>Max count of unique symbols</returns>
-        static int GetMaxCountOfUniqueSequenceOfLetters(string sequenceOfCounting)
-        {
-            if (string.IsNullOrEmpty(sequenceOfCounting))
-            {
-                return 0;
-            }
-
-            int count = 1;
-            int maxCount = 1;
-
-            for(int i = 0; i < sequenceOfCounting.Length-1; i++)
-            {
-                if (sequenceOfCounting[i] != sequenceOfCounting[i + 1])
-                {
-                    count++;
-                }
-
-                if (sequenceOfCounting[i] == sequenceOfCounting[i + 1])
-                {
-                    if (count > maxCount)
-                    {
-                        maxCount = count;
-                    }
-                    count = 1;
-                }
-            }
-
-            if (count > maxCount)
-            {
-                maxCount = count;
-            }
-
-            return maxCount;
         }
     }
 }
