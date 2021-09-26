1e4e81e03cd25b32ef1fd0aafe49d632ef73c7c7
Kirill Novik <thenavik1337@gmail.com>
2021-09-26 20:53:09 +0300

    Add autorization page

--- MailWebDriver/MailWebDriver/MailRuModel/AutorizationPageObject.cs
+++ MailWebDriver/MailWebDriver/MailRuModel/AutorizationPageObject.cs
@@ -1,10 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using OpenQA.Selenium;
+using System;
 using System.Text;
+using System.Threading;
 
-namespace MailWebDriver.MailRuModel
+namespace WebDriver.MailRuModel
 {
-    class AutorizationPageObject
+    public class AutorizationPageObject
     {
+        private IWebDriver _webDriver;
+
+        private readonly By _accountNameInput = By.XPath("//input[@name='login']");
+        private readonly By _furtherButton = By.XPath("//button[@data-testid='enter-password']");
+        private readonly By _passwordInput = By.XPath("//input[@name='password']");
+        private readonly By _signInButton = By.XPath("//button[@data-testid='login-to-mail']");
+        private readonly By _errorMessage = By.XPath("//div[@class='error svelte-1tib0qz']");
+
+        private const string _path = "https://mail.ru/";
+
+        public AutorizationPageObject(IWebDriver webDriver)
+        {
+            _webDriver = webDriver;
+            _webDriver.Manage().Window.Maximize();
+            _webDriver.Navigate().GoToUrl(_path);
+        }
+
+        public AutorizationPageObject Login(string login, string password)
+        {
+            _webDriver.FindElement(_accountNameInput).SendKeys(login);
+            _webDriver.FindElement(_furtherButton).Click();
+            Thread.Sleep(10000);
+            _webDriver.FindElement(_passwordInput).SendKeys(password);
+            _webDriver.FindElement(_signInButton).Click();
+
+            return this;
+        }
+
+        public bool IsErrorDisplayd()
+        {
+            Thread.Sleep(10000);
+            return _webDriver.FindElement(_errorMessage).Displayed;
+        }
     }
 }
--- MailWebDriver/MailWebDriverTests/UnitTest1.cs
+++ MailWebDriver/MailWebDriverTests/UnitTest1.cs
@@ -1,18 +1,29 @@
 using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using WebDriver.MailRuModel;
 
 namespace MailWebDriverTests
 {
     public class Tests
     {
+        private IWebDriver _webDriver;
+
         [SetUp]
         public void Setup()
         {
+            _webDriver = new ChromeDriver();
         }
 
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            var mainPage = new AutorizationPageObject(_webDriver);
+            mainPage.Login("alisap_etrova1992", "bgtvfrcdexswzaq1");
+
+            bool check = mainPage.IsErrorDisplayd();
+
+            Assert.IsTrue(check);
         }
     }
 }
