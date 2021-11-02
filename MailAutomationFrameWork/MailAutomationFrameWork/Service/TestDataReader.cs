27b65e20080efc497a3f0d005f019eec8f0e1679
Kirill Novik <thenavik1337@gmail.com>
2021-11-02 23:57:38 +0300

    refactoring

--- MailAutomationFrameWork/MailAutomationFrameWork/Service/TestDataReader.cs
+++ MailAutomationFrameWork/MailAutomationFrameWork/Service/TestDataReader.cs
@@ -7,22 +7,27 @@ using NUnit.Framework;
 
 namespace MailAutomationFrameWork.Service
 {
-    public static class TestDataReader
+    public class TestDataReader
     {
+        private static string _filePath = "../../../../MailAutomationFrameWork/Resources/";
         private static string _environment = TestContext.Parameters["environment"];
-        private static string _filePath = "Resources/";
 
-        public static string GetValue(string key)
+        public static string GetTestData(string key)
         {
-            var objectJsonFile = File.ReadAllText($"{_filePath}{_environment}.json");
-            JObject json = JObject.Parse(objectJsonFile);
+            return GetValue(_environment, key);
+        }
+
+        private static string GetValue(string environment, string key)
+        {
+            var text = File.ReadAllText($"{_filePath}{environment}.json");
+            JObject json = JObject.Parse(text);
 
             return json.Descendants()
-                .OfType<JProperty>()
-                .Where(k => k.Name == key)
-                .First()
-                .Value
-                .ToString();
+                    .OfType<JProperty>()
+                    .Where(p => p.Name == key)
+                    .First()
+                    .Value
+                    .ToString();
         }
     }
 }
--- MailAutomationFrameWork/MailAutomationFrameWork/Service/UserCreator.cs
+++ MailAutomationFrameWork/MailAutomationFrameWork/Service/UserCreator.cs
@@ -11,10 +11,10 @@ namespace MailAutomationFrameWork.Service
         public static readonly string TestDataYandexPass = "testdata.yandex.user.password";
 
         public static User UserMailRu
-            => new User(TestDataReader.GetValue(TestDataMailRuName), TestDataReader.GetValue(TestDataMailRuPass));
+            => new User(TestDataReader.GetTestData(TestDataMailRuName), TestDataReader.GetTestData(TestDataMailRuPass));
 
         public static User UserYandex
-            => new User(TestDataReader.GetValue(TestDataYandexName), TestDataReader.GetValue(TestDataYandexPass));
+            => new User(TestDataReader.GetTestData(TestDataYandexName), TestDataReader.GetTestData(TestDataYandexPass));
 
         public static User UserEmptyMailRU
             => new User(string.Empty, string.Empty);
@@ -23,10 +23,10 @@ namespace MailAutomationFrameWork.Service
             => new User("dfvpodrnpk12-sckvfir@mail.ru", string.Empty);
 
         public static User UserInvalidPasswordMailRu
-            => new User(TestDataReader.GetValue(TestDataMailRuName), TestDataReader.GetValue(TestDataYandexPass));
+            => new User(TestDataReader.GetTestData(TestDataMailRuName), TestDataReader.GetTestData(TestDataYandexPass));
 
         public static User UserEmptyPassword
-            => new User(TestDataReader.GetValue(TestDataMailRuName), string.Empty);
+            => new User(TestDataReader.GetTestData(TestDataMailRuName), string.Empty);
 
 
 
